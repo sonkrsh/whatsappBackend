f43afdc2ffccbdb78f6f55e354d4ee16dbcfe3be
Sourav <sourav.singh@emoha.com>
2022-12-07 18:16:11 +0530

    fix: removed commented code

--- src/validations/users.validation.js
+++ src/validations/users.validation.js
@@ -18,45 +18,7 @@ const login = {
   }),
 };
 
-// const logout = {
-//   body: Joi.object().keys({
-//     refreshToken: Joi.string().required(),
-//   }),
-// };
-
-// const refreshTokens = {
-//   body: Joi.object().keys({
-//     refreshToken: Joi.string().required(),
-//   }),
-// };
-
-// const forgotPassword = {
-//   body: Joi.object().keys({
-//     email: Joi.string().email().required(),
-//   }),
-// };
-
-// const resetPassword = {
-//   query: Joi.object().keys({
-//     token: Joi.string().required(),
-//   }),
-//   body: Joi.object().keys({
-//     password: Joi.string().required().custom(password),
-//   }),
-// };
-
-// const verifyEmail = {
-//   query: Joi.object().keys({
-//     token: Joi.string().required(),
-//   }),
-// };
-
 module.exports = {
   register,
   login,
-  //   logout,
-  //   refreshTokens,
-  //   forgotPassword,
-  //   resetPassword,
-  //   verifyEmail,
 };
